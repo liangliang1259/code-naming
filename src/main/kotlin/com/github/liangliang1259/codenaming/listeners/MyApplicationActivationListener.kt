305abda506b5ef95640037ee597d63c4075bdbf1
GitHub Action <action@github.com>
2024-09-12 03:09:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/liangliang1259/codenaming/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/liangliang1259/codenaming/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.liangliang1259.codenaming.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/liangliang1259/codenaming/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/liangliang1259/codenaming/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.liangliang1259.codenaming.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.liangliang1259.codenaming.MyBundle
+import com.github.liangliang1259.codenaming.services.MyProjectService
 import javax.swing.JButton
 
 
